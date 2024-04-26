eaaa921f05749064c473371fa656766faf59776a
Blayung <gpewojan1@gmail.com>
2024-04-26 23:02:31 +0200

    renamed html-gui-server to server-list; various server list changes including dark mode and adding options not to show errorneous entries and forge data; modified the README.md a little bit

--- README.md
+++ README.md
@@ -6,5 +6,5 @@
 ## Usage:
 1. Scan the internet for open 25565 ports using masscan and put the results in the list format in `data/masscan-list.txt` (you can use the script in `data/start-masscan.sh`).
 2. Start the pinger with `cargo run --bin pinger` (the server list will be put in `data/server-list.dat`).
-3. Start the http server allowing for easy server list browsing with `cargo run --bin html-gui-server`.
-4. View the server list by going to `http://127.0.0.1:8888` in your browser.
+3. Start the http server with the server list with `cargo run --bin server-list`.
+4. View the server list by going to `http://localhost:8888` in your browser.
--- src/server-list.rs
+++ src/server-list.rs
@@ -18,31 +18,31 @@ async fn main() {
 
         let mut server_list_html = String::new();
         for entry in server_list {
-            server_list_html.push_str("<tr><th>");
+            server_list_html.push_str("<tr><td>");
             server_list_html.push_str(&entry.ip);
-            server_list_html.push_str("</th><th>");
+            server_list_html.push_str("</td><td>");
             server_list_html.push_str(&entry.port);
-            server_list_html.push_str("</th><th>");
+            server_list_html.push_str("</td><td>");
             match entry.ping_response {
                 None => server_list_html.push_str("ERROR"),
                 Some(ping_response) => {
-                    server_list_html.push_str("OK</th><th>");
+                    server_list_html.push_str("OK</td><td>");
                     server_list_html.push_str(&html_escape::encode_text(&ping_response.version));
-                    server_list_html.push_str("</th><th>");
+                    server_list_html.push_str("</td><td>");
                     server_list_html.push_str(&ping_response.protocol.to_string());
-                    server_list_html.push_str("</th><th>");
+                    server_list_html.push_str("</td><td>");
                     if let Some(favicon) = ping_response.favicon {
                         server_list_html.push_str("<img src='data:image/png;base64,");
                         server_list_html.push_str(&base64::prelude::BASE64_STANDARD.encode(favicon));
                         server_list_html.push_str("'>");
                     }
-                    server_list_html.push_str("</th><th>");
+                    server_list_html.push_str("</td><td>");
                     server_list_html.push_str(&html_escape::encode_text(&mc_text_to_string(ping_response.description)));
-                    server_list_html.push_str("</th><th>");
+                    server_list_html.push_str("</td><td>");
                     server_list_html.push_str(&ping_response.online_players.to_string());
-                    server_list_html.push_str("</th><th>");
+                    server_list_html.push_str("</td><td>");
                     server_list_html.push_str(&ping_response.max_players.to_string());
-                    server_list_html.push_str("</th><th>");
+                    server_list_html.push_str("</td><td>");
                     if let Some(sample) = ping_response.sample {
                         if let Some(player) = sample.first() {
                             server_list_html.push_str(&html_escape::encode_text(&player.name));
@@ -50,7 +50,7 @@ async fn main() {
                     } else {
                         server_list_html.push_str("?");
                     }
-                    server_list_html.push_str("</th><th>");
+                    server_list_html.push_str("</td><td>");
                     server_list_html.push_str(match ping_response.enforces_secure_chat {
                         None => "?",
                         Some(enforces_secure_chat) => if enforces_secure_chat {
@@ -59,7 +59,7 @@ async fn main() {
                             "Doesn't enforce"
                         }
                     });
-                    server_list_html.push_str("</th><th>");
+                    server_list_html.push_str("</td><td>");
                     server_list_html.push_str(match ping_response.previews_chat {
                         None => "?",
                         Some(previews_chat) => if previews_chat {
@@ -68,19 +68,19 @@ async fn main() {
                             "Doesn't preview"
                         }
                     });
-                    server_list_html.push_str("</th><th>");
+                    server_list_html.push_str("</td><td>");
                     if let Some(forge_data) = ping_response.forge_data {
                         let forge_data = format!("{:?}", forge_data);
                         server_list_html.push_str(&html_escape::encode_text(&forge_data));
                     }
-                    server_list_html.push_str("</th><th>");
+                    server_list_html.push_str("</td><td>");
                     if let Some(mod_info) = ping_response.mod_info {
                         let mod_info = format!("{:?}", mod_info);
                         server_list_html.push_str(&html_escape::encode_text(&mod_info));
                     }
                 }
             }
-            server_list_html.push_str("</th></tr>");
+            server_list_html.push_str("</td></tr>");
         }
 
         actix_web::HttpResponse::Ok().body(format!(include_str!("server-list.html"), server_list_html))
