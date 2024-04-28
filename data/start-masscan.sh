945471858afbd9ce53a26c2add6b6634dcee55b0
Blayung <gpewojan1@gmail.com>
2024-04-28 11:45:14 +0200

    decreased the default masscan packet rate

--- data/start-masscan.sh
+++ data/start-masscan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo masscan -p25565 0.0.0.0/0 --exclude 255.255.255.255 --rate 500000 -oL masscan-list.txt
+sudo masscan -p25565 0.0.0.0/0 --exclude 255.255.255.255 --rate 200000 -oL masscan-list.txt
