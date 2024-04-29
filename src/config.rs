a516e6fce270ef4b8fcd390fb8d87faa609b0f31
Blayung <gpewojan1@gmail.com>
2024-04-29 17:39:37 +0200

    changed the default server address to allow connections outside localhost

--- src/config.rs
+++ src/config.rs
@@ -3,4 +3,4 @@ pub const SERVER_LIST_PATH: &str = "./data/server-list";
 pub const MASSCAN_LIST_PATH: &str = "./data/masscan-list.txt";
 pub const PING_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(1);
 
-pub const SERVER_ADDRESS: &str = "127.0.0.1:8888";
+pub const SERVER_ADDRESS: &str = "0.0.0.0:8888";
